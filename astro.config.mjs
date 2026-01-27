bba8cb49993104ba39a95092ad802520feff5cdd
AstyanM <astyanmartinpbl@gmail.com>
2026-01-27 23:47:40 +0100

    Revert "Update astro.config.mjs"
    This reverts commit 71d26dff2e8614d17c56305a9d0463fa53abd534.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import { rehypeFigureCaption } from "./src/plugins/rehype-figure-caption.mjs";
-import { rehypeVideo } from "./src/plugins/rehype-video.mjs";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import { rehypeFigureCaption } from './src/plugins/rehype-figure-caption.mjs';
+import { rehypeVideo } from './src/plugins/rehype-video.mjs';
 
 // Configuration Astro
 export default defineConfig({
-  site: "https://astyanm.github.io/",
-  base: "/portfolio",
+  site: 'https://astyanm.github.io/',
   integrations: [
     react(),
     tailwind({
@@ -20,22 +19,22 @@ export default defineConfig({
   ],
   markdown: {
     shikiConfig: {
-      theme: "github-dark",
+      theme: 'github-dark',
       wrap: true,
     },
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex, rehypeFigureCaption, rehypeVideo],
   },
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"],
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
     routing: {
       prefixDefaultLocale: true,
     },
   },
   image: {
     service: {
-      entrypoint: "astro/assets/services/sharp",
+      entrypoint: 'astro/assets/services/sharp',
     },
   },
 });
